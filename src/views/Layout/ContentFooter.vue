866c6971151352d0edadbbfe66db85c0a9b3f3cc
simjaeseo <delpho@naver.com>
2022-05-20 13:39:50 +0900

    login.vue 수정

--- src/views/Layout/ContentFooter.vue
+++ src/views/Layout/ContentFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="footer px-4">
-    <b-row align-v="center" class="justify-content-lg-between">
+    <!-- <b-row align-v="center" class="justify-content-lg-between">
       <b-col lg="6">
         <div class="copyright text-center text-lg-left text-muted">
           © {{year}} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
@@ -22,7 +22,7 @@
           </b-nav-item>
         </b-nav>
       </b-col>
-    </b-row>
+    </b-row> -->
   </footer>
 </template>
 <script>
--- src/views/Pages/Register.vue
+++ src/views/Pages/Register.vue
@@ -6,17 +6,26 @@
         <div class="header-body text-center mb-7">
           <b-row class="justify-content-center">
             <b-col xl="5" lg="6" md="8" class="px-5">
-              <h1 class="text-white">Create an account</h1>
-              <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for
-                free.</p>
+              <h1 class="text-white">회원가입</h1>
+              <!-- <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for
+                free.</p> -->
             </b-col>
           </b-row>
         </div>
       </b-container>
       <div class="separator separator-bottom separator-skew zindex-100">
-        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
-             xmlns="http://www.w3.org/2000/svg">
-          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        <svg
+          x="0"
+          y="0"
+          viewBox="0 0 2560 100"
+          preserveAspectRatio="none"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polygon
+            class="fill-default"
+            points="2560 0 2560 100 0 100"
+          ></polygon>
         </svg>
       </div>
     </div>
@@ -24,67 +33,131 @@
     <b-container class="mt--8 pb-5">
       <!-- Table -->
       <b-row class="justify-content-center">
-        <b-col lg="6" md="8" >
+        <b-col lg="6" md="8">
           <b-card no-body class="bg-secondary border-0">
             <b-card-header class="bg-transparent pb-5">
-              <div class="text-muted text-center mt-2 mb-4"><small>Sign up with</small></div>
+              <div class="text-muted text-center mt-2 mb-4">
+                <!-- <small>Sign up with</small> -->
+              </div>
               <div class="text-center">
                 <a href="#" class="btn btn-neutral btn-icon mr-4">
-                  <span class="btn-inner--icon"><img src="img/icons/common/github.svg"></span>
-                  <span class="btn-inner--text">Github</span>
+                  <span class="btn-inner--icon"
+                    ><img src="img/icons/common/kakaologo.jpg"
+                  /></span>
+                  <span class="btn-inner--text">카카오톡으로 회원가입</span>
                 </a>
-                <a href="#" class="btn btn-neutral btn-icon">
-                  <span class="btn-inner--icon"><img src="img/icons/common/google.svg"></span>
+                <!-- <a href="#" class="btn btn-neutral btn-icon">
+                  <span class="btn-inner--icon"
+                    ><img src="img/icons/common/google.svg"
+                  /></span>
                   <span class="btn-inner--text">Google</span>
-                </a>
+                </a> -->
               </div>
             </b-card-header>
             <b-card-body class="px-lg-5 py-lg-5">
               <div class="text-center text-muted mb-4">
-                <small>Or sign up with credentials</small>
+                <small>다른 방법으로 회원가입</small>
               </div>
-              <validation-observer v-slot="{handleSubmit}" ref="formValidator">
+              <validation-observer
+                v-slot="{ handleSubmit }"
+                ref="formValidator"
+              >
                 <b-form role="form" @submit.prevent="handleSubmit(onSubmit)">
-                  <base-input alternative
-                              class="mb-3"
-                              prepend-icon="ni ni-hat-3"
-                              placeholder="Name"
-                              name="Name"
-                              :rules="{required: true}"
-                              v-model="model.name">
+                  <base-input
+                    alternative
+                    class="mb-3"
+                    prepend-icon="ni ni-hat-3"
+                    placeholder="id"
+                    name="id"
+                    :rules="{ required: true }"
+                    v-model="model.id"
+                  >
                   </base-input>
 
-                  <base-input alternative
-                              class="mb-3"
-                              prepend-icon="ni ni-email-83"
-                              placeholder="Email"
-                              name="Email"
-                              :rules="{required: true, email: true}"
-                              v-model="model.email">
+                  <base-input
+                    alternative
+                    class="mb-3"
+                    prepend-icon="ni ni-lock-circle-open"
+                    placeholder="password"
+                    type="password"
+                    name="Password"
+                    :rules="{ required: true, min: 6 }"
+                    v-model="model.password"
+                  >
                   </base-input>
 
-                  <base-input alternative
-                              class="mb-3"
-                              prepend-icon="ni ni-lock-circle-open"
-                              placeholder="password"
-                              type="password"
-                              name="Password"
-                              :rules="{required: true, min: 6}"
-                              v-model="model.password">
+                  <base-input
+                    alternative
+                    class="mb-3"
+                    prepend-icon="ni ni-hat-3"
+                    placeholder="Name"
+                    name="Name"
+                    :rules="{ required: true }"
+                    v-model="model.name"
+                  >
                   </base-input>
-                  <div class="text-muted font-italic"><small>password strength: <span
-                    class="text-success font-weight-700">strong</span></small></div>
+
+                  <base-input
+                    alternative
+                    class="mb-3"
+                    prepend-icon="ni ni-email-83"
+                    placeholder="Email"
+                    name="Email"
+                    :rules="{ required: true, email: true }"
+                    v-model="model.email"
+                  >
+                  </base-input>
+
+                  <base-input
+                    alternative
+                    class="mb-3"
+                    prepend-icon="ni ni-email-83"
+                    placeholder="phone"
+                    name="phone"
+                    :rules="{ required: true }"
+                    v-model="model.phone"
+                  >
+                  </base-input>
+
+                  <base-input
+                    alternative
+                    class="mb-3"
+                    prepend-icon="ni ni-email-83"
+                    placeholder="favorite"
+                    name="favorite"
+                    :rules="{ required: true }"
+                    v-model="model.favorite"
+                  >
+                  </base-input>
+
+                  <!-- <div class="text-muted font-italic">
+                    <small
+                      >password strength:
+                      <span class="text-success font-weight-700"
+                        >strong</span
+                      ></small
+                    >
+                  </div> -->
                   <b-row class=" my-4">
                     <b-col cols="12">
-                      <base-input :rules="{ required: { allowFalse: false } }" name=Privacy Policy>
+                      <!-- <base-input
+                        :rules="{ required: { allowFalse: false } }"
+                        name="Privacy"
+                        Policy
+                      >
                         <b-form-checkbox v-model="model.agree">
-                          <span class="text-muted">I agree with the <a href="#!">Privacy Policy</a></span>
+                          <span class="text-muted"
+                            >I agree with the
+                            <a href="#!">Privacy Policy</a></span
+                          >
                         </b-form-checkbox>
-                      </base-input>
+                      </base-input> -->
                     </b-col>
                   </b-row>
                   <div class="text-center">
-                    <b-button type="submit" variant="primary" class="mt-4">Create account</b-button>
+                    <b-button type="submit" variant="primary" class="mt-4"
+                      >회원가입</b-button
+                    >
                   </div>
                 </b-form>
               </validation-observer>
@@ -96,25 +169,30 @@
   </div>
 </template>
 <script>
-
-  export default {
-    name: 'register',
-    data() {
-      return {
-        model: {
-          name: '',
-          email: '',
-          password: '',
-          agree: false
-        }
+import http from "@/api/http";
+export default {
+  name: "register",
+  data() {
+    return {
+      model: {
+        id: "",
+        name: "",
+        adress: "",
+        phone: "",
+        favorite: "",
+        password: ""
+        // agree: false
       }
-    },
-    methods: {
-      onSubmit() {
-        // this will be called only after form is valid. You can do an api call here to register users
-      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      http.post("/singUp", model).then(res => {
+        console.log(res);
+      });
+      // this will be called only after form is valid. You can do an api call here to register users
     }
-
-  };
+  }
+};
 </script>
 <style></style>
