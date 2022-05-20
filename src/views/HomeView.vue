315c680d52a4747d75abb8f2d6918cc14d9b4fd4
jiseok <cjswltjr159@naver.com>
2022-05-20 09:47:12 +0900

    map init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,65 +1,6 @@
 <template>
   <div>
     <base-header class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-success">
-      <!-- Card stats -->
-      <!-- <b-row>
-        <b-col xl="3" md="6">
-          <stats-card
-            title="Total traffic"
-            type="gradient-red"
-            sub-title="350,897"
-            icon="ni ni-active-40"
-            class="mb-4"
-          >
-            <template slot="footer">
-              <span class="text-success mr-2">3.48%</span>
-              <span class="text-nowrap">Since last month</span>
-            </template>
-          </stats-card>
-        </b-col>
-        <b-col xl="3" md="6">
-          <stats-card
-            title="Total traffic"
-            type="gradient-orange"
-            sub-title="2,356"
-            icon="ni ni-chart-pie-35"
-            class="mb-4"
-          >
-            <template slot="footer">
-              <span class="text-success mr-2">12.18%</span>
-              <span class="text-nowrap">Since last month</span>
-            </template>
-          </stats-card>
-        </b-col>
-        <b-col xl="3" md="6">
-          <stats-card
-            title="Sales"
-            type="gradient-green"
-            sub-title="924"
-            icon="ni ni-money-coins"
-            class="mb-4"
-          >
-            <template slot="footer">
-              <span class="text-danger mr-2">5.72%</span>
-              <span class="text-nowrap">Since last month</span>
-            </template>
-          </stats-card>
-        </b-col>
-        <b-col xl="3" md="6">
-          <stats-card
-            title="Performance"
-            type="gradient-info"
-            sub-title="49,65%"
-            icon="ni ni-chart-bar-32"
-            class="mb-4"
-          >
-            <template slot="footer">
-              <span class="text-success mr-2">54.8%</span>
-              <span class="text-nowrap">Since last month</span>
-            </template>
-          </stats-card>
-        </b-col>
-      </b-row> -->
     </base-header>
 
     <b-container fluid class="mt--7">
@@ -79,95 +20,37 @@
 </template>
 <script>
 import { API_KEY } from "./Maps/API_KEY";
-import GoogleMapsLoader from "google-maps";
-
-GoogleMapsLoader.KEY = API_KEY;
 
 export default {
   methods: {
-    initMap(google) {
-      let map,
-        lat = 40.748817,
-        lng = -73.985428,
-        color = "#5e72e4";
-      map = document.getElementById("map-custom");
-
-      let myLatlng = new google.maps.LatLng(lat, lng);
-      let mapOptions = {
-        zoom: 12,
-        scrollwheel: false,
-        center: myLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles: [
-          {
-            featureType: "administrative",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#444444" }]
-          },
-          {
-            featureType: "landscape",
-            elementType: "all",
-            stylers: [{ color: "#f2f2f2" }]
-          },
-          {
-            featureType: "poi",
-            elementType: "all",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "road",
-            elementType: "all",
-            stylers: [{ saturation: -100 }, { lightness: 45 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "all",
-            stylers: [{ visibility: "simplified" }]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "labels.icon",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "transit",
-            elementType: "all",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "water",
-            elementType: "all",
-            stylers: [{ color: color }, { visibility: "on" }]
-          }
-        ]
+    initMap() {
+      const container = document.querySelector("#map-custom");
+      const options = {
+        center: new kakao.maps.LatLng(35.19656853772262, 129.0807270648317),
+        level: 3
       };
+      const map = new kakao.maps.Map(container, options);
+      // const markerPosition = new kakao.maps.LatLng(
+      //   35.19656853772262,
+      //   129.0807270648317
+      // );
 
-      map = new google.maps.Map(map, mapOptions);
-
-      let marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        title: "Hello World!"
-      });
-
-      let contentString =
-        '<div class="info-window-content"><h2>Argon Dashboard PRO</h2>' +
-        "<p>A beautiful premium dashboard for Bootstrap 4.</p></div>";
-
-      let infowindow = new google.maps.InfoWindow({
-        content: contentString
-      });
-
-      google.maps.event.addListener(marker, "click", function() {
-        infowindow.open(map, marker);
-      });
+      // const marker = new kakao.maps.Marker({
+      //   position: markerPosition
+      // });
+      // marker.setMap(map);
     }
   },
   mounted() {
-    GoogleMapsLoader.load(google => {
-      this.initMap(google);
-    });
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement("script");
+      script.onload = () => kakao.maps.load(this.initMap);
+
+      script.src = `http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${API_KEY}`;
+      document.head.appendChild(script);
+    }
   }
 };
 </script>
